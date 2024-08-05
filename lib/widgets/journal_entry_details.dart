4208317261eb6c38a50b3270796e9ed1c0a25d8f
davidfitz314 <davidfitz314@gmail.com>
2024-08-05 04:09:15 +0000

    clean up

--- lib/widgets/journal_entry_details.dart
+++ lib/widgets/journal_entry_details.dart
@@ -4,7 +4,7 @@ import 'package:myapp/widgets/background_gradient.dart';
 class JournalEntryDetails extends StatelessWidget {
   final Map<String, dynamic> entry;
 
-  const JournalEntryDetails({required this.entry});
+  const JournalEntryDetails({super.key, required this.entry});
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +12,12 @@ class JournalEntryDetails extends StatelessWidget {
 
     return Scaffold(
         appBar: AppBar(
-          title:
-              Text(entry['createdDate'].toString().split(" ")[0] ?? 'Details'),
-          backgroundColor: Color(0xFFE0F7FA).withOpacity(.8),
+          title: Text(entry['createdDate'].toString().split(" ")[0]),
+          backgroundColor: const Color(0xFFE0F7FA).withOpacity(.8),
         ),
         body: Stack(
           children: [
-            GradientBg(),
+            const GradientBg(),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
@@ -28,7 +27,7 @@ class JournalEntryDetails extends StatelessWidget {
                     "My Mood: ${entry['title'].toString().split(' ').join(', ')}",
                     style: Theme.of(context).textTheme.headlineSmall,
                   ),
-                  SizedBox(height: 16),
+                  const SizedBox(height: 16),
                   Text(
                     dateTime.toString(),
                     style: Theme.of(context).textTheme.bodySmall,
