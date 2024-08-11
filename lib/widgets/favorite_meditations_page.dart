9a9b109a09e99647748e766316b764bbf31d0ca5
davidfitz314 <davidfitz314@gmail.com>
2024-08-11 22:28:35 +0000

    add delete buttons

--- lib/widgets/favorite_meditations_page.dart
+++ lib/widgets/favorite_meditations_page.dart
@@ -4,26 +4,45 @@ import 'package:myapp/db/database_service.dart';
 import 'package:myapp/widgets/background_gradient.dart';
 import 'package:myapp/widgets/meditation_details_page.dart';
 
-class FavoriteMeditationsPage extends ConsumerWidget {
+class FavoriteMeditationsPage extends ConsumerStatefulWidget {
   const FavoriteMeditationsPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final databaseService = DatabaseMeditationService();
+  _FavoriteMeditationsPageState createState() =>
+      _FavoriteMeditationsPageState();
+}
 
+class _FavoriteMeditationsPageState
+    extends ConsumerState<FavoriteMeditationsPage> {
+  late Future<List<Map<String, dynamic>>> _favoriteMeditations;
+
+  @override
+  void initState() {
+    super.initState();
+    _favoriteMeditations = _loadFavoriteMeditations();
+  }
+
+  Future<List<Map<String, dynamic>>> _loadFavoriteMeditations() async {
+    final databaseService = DatabaseMeditationService();
+    return databaseService.getEntries();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           const GradientBg(),
           FutureBuilder<List<Map<String, dynamic>>>(
-            future: databaseService.getEntries(),
+            future: _favoriteMeditations,
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
+                return const Center(child: CircularProgressIndicator());
               } else if (snapshot.hasError) {
                 return Center(child: Text('Error: ${snapshot.error}'));
               } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return Center(child: Text('No favorite meditations found.'));
+                return const Center(
+                    child: Text('No favorite meditations found.'));
               } else {
                 final entries = snapshot.data!;
 
@@ -31,8 +50,7 @@ class FavoriteMeditationsPage extends ConsumerWidget {
                   itemCount: entries.length,
                   itemBuilder: (context, index) {
                     final entry = entries[index];
-                    final title = entry['title'] ??
-                        'No Title'; // Use title from entry or default
+                    final title = entry['title'] ?? 'No Title';
                     final steps = entry['steps'] as List<dynamic>? ?? [];
                     final preview =
                         steps.take(4).map((step) => step.toString()).join('\n');
@@ -69,8 +87,50 @@ class FavoriteMeditationsPage extends ConsumerWidget {
                               ),
                             ),
                           );
-                          // Handle tap to view full meditation steps, if desired
                         },
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () async {
+                            // Show a confirmation dialog
+                            bool? confirmDelete = await showDialog<bool>(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: Text('Delete Meditation'),
+                                  content: Text(
+                                      'Are you sure you want to delete this meditation?'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(false);
+                                      },
+                                      child: Text('Cancel'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(true);
+                                      },
+                                      child: Text('Delete'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+
+                            if (confirmDelete == true) {
+                              // Delete the entry from Firebase
+                              final databaseService =
+                                  DatabaseMeditationService();
+                              await databaseService.deleteEntry(entry['id']);
+
+                              // Refresh the list by reloading the data
+                              setState(() {
+                                _favoriteMeditations =
+                                    _loadFavoriteMeditations();
+                              });
+                            }
+                          },
+                        ),
                       ),
                     );
                   },
